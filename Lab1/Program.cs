42ce8231943566f60ebabc2c8706f217bd750dee
Aleksey Sirko <leshasirko@gmail.com>
2022-11-18 02:05:46 +0300

    Added UI to Lab1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,30 +1,72 @@
 ﻿using Lab1;
 
-Console.WriteLine("Для подсчёта требуется ввод данных:");
+internal class Program
+{
+    private static double _moltenIronVolume;
+    private static double _moltenSiliconWeight;
+    private static double _ironConcentration;
+    private static double _crystallizedPrecentageP;
+    private static double _crystallizedPrecentageR;
 
-    Console.Write("Материала тигля расстворилось (X [см^3]): ");
-    double moltenIronVolume = Convert.ToDouble(Console.ReadLine());
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Для подсчёта требуется ввод данных:");
+        Console.WriteLine("Нажмите [1], чтобы выбрать значения варианта 25. Или [AnyKey], чтобы ввести значения вручную");
+        ConsoleKeyInfo readKey = Console.ReadKey(intercept: true);
+        if (readKey.Key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+        {
+            SetVariantValues();
+        }
+        else
+        {
+            InputManually();
+        }
 
-    Console.Write("Вес расплава кремния (Y [г]): ");
-    double moltenSiliconWeight = Convert.ToDouble(Console.ReadLine());
+        Tigel crusible = new(_moltenIronVolume, _ironConcentration);
+        Silicon silicon = new(_moltenSiliconWeight, _crystallizedPrecentageP, _crystallizedPrecentageR);
 
-    Console.Write("Концентрация железа в материале (Z [см^-3]): ");
-    double ironConcentration = Convert.ToDouble(Console.ReadLine());
+        double initialIronConcentration = ConcentrationCalculator.GetInitialIronConcentrationInMelt(crusible, silicon);
+        double beginConcentrationInSolidPhase = ConcentrationCalculator.GetBeginConcentrationInSolidPhase(crusible, silicon);
+        double tailConcentrationInSolidPhase = ConcentrationCalculator.GetTailConcentrationInSolidPhase(crusible, silicon);
 
-    Console.Write("Доля закристаллизовавшегося расплава в затравочной части (Р [%]): ");
-    double crystallizedPrecentageP = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("\nРезультаты подсчётов:");
+        Console.WriteLine($"Первоначальная концентрация примеси в расплаве = {initialIronConcentration}");
+        Console.WriteLine($"Концентрацию железа в затравочной части слитка = {beginConcentrationInSolidPhase}");
+        Console.WriteLine($"Концентрацию железа в хвостовой части слитка = {tailConcentrationInSolidPhase}");
+    }
 
-    Console.Write("Доля в хвостовой части (R [%]): ");
-    double crystallizedPrecentageR = Convert.ToDouble(Console.ReadLine());
 
-Tigel crusible = new(moltenIronVolume, ironConcentration);
-Silicon silicon = new(moltenSiliconWeight, crystallizedPrecentageP, crystallizedPrecentageR);
+    private static void SetVariantValues()
+    {
+        const double VariantMoltenIronVolume = 540;
+        const double VariantMoltenSiliconWeight = 16740;
+        const double VariantIronConcentration = 4.4;
+        const double VariantCrystallizedPrecentageP = 0.12;
+        const double VariantCrystallizedPrecentageR = 0.66;
 
-double initialIronConcentration = ConcentrationCalculator.GetInitialIronConcentrationInMelt(crusible, silicon);
-double beginConcentrationInSolidPhase = ConcentrationCalculator.GetBeginConcentrationInSolidPhase(crusible, silicon);
-double tailConcentrationInSolidPhase = ConcentrationCalculator.GetTailConcentrationInSolidPhase(crusible, silicon);
+        Console.WriteLine($"Материала тигля расстворилось (X [см^3]) = {VariantMoltenIronVolume}");
+        _moltenIronVolume = VariantMoltenIronVolume;
+        Console.WriteLine($"Вес расплава кремния (Y [г]) = {VariantMoltenSiliconWeight}");
+        _moltenSiliconWeight = VariantMoltenSiliconWeight;
+        Console.WriteLine($"Концентрация железа в материале (Z [см^-3]) = {VariantIronConcentration}");
+        _ironConcentration = VariantIronConcentration;
+        Console.WriteLine($"Доля закристаллизовавшегося расплава в затравочной части (Р [%]) = {VariantCrystallizedPrecentageP}");
+        _crystallizedPrecentageP = VariantCrystallizedPrecentageP;
+        Console.WriteLine($"Доля в хвостовой части (R [%]) = {VariantCrystallizedPrecentageR}");
+        _crystallizedPrecentageR = VariantCrystallizedPrecentageR;
+    }
 
-Console.WriteLine("\nРезультаты подсчётов:");
-Console.WriteLine($"Первоначальная концентрация примеси в расплаве = {initialIronConcentration}");
-Console.WriteLine($"Концентрацию железа в затравочной части слитка = {beginConcentrationInSolidPhase}");
-Console.WriteLine($"Концентрацию железа в хвостовой части слитка = {tailConcentrationInSolidPhase}");
+    private static void InputManually()
+    {
+        Console.Write("Материала тигля расстворилось (X [см^3]): ");
+        _moltenIronVolume = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Вес расплава кремния (Y [г]): ");
+        _moltenSiliconWeight = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Концентрация железа в материале (Z [см^-3]): ");
+        _ironConcentration = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Доля закристаллизовавшегося расплава в затравочной части (Р [%]): ");
+        _crystallizedPrecentageP = Convert.ToDouble(Console.ReadLine());
+        Console.Write("Доля в хвостовой части (R [%]): ");
+        _crystallizedPrecentageR = Convert.ToDouble(Console.ReadLine());
+    }
+}
