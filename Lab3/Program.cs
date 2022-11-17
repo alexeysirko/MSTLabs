c138bc105d3f836d2fdb3eab72e56bd1a442eae6
Aleksey Sirko <leshasirko@gmail.com>
2022-11-18 02:23:26 +0300

    Created Lab3 project, moved each project to .Net 7

---
+++ Lab3/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    private static double 
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Для подсчёта требуется ввод данных");
+        Console.WriteLine("Нажмите [1], чтобы выбрать значения варианта 25. Или [AnyKey], чтобы ввести значения вручную");
+
+        ConsoleKeyInfo readKey = Console.ReadKey(intercept: true);
+        if (readKey.Key is ConsoleKey.D1 or ConsoleKey.NumPad1)
+        {
+            SetVariantValues();
+        }
+        else
+        {
+            InputManually();
+        }
+    }
+
+    private static void InputManually()
+    {
+        
+    }
+
+    private static void SetVariantValues()
+    {
+        
+    }
+}
