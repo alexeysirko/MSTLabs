490b7c9e9884da92810329382ec969b458baf7e9
Aleksey Sirko <leshasirko@gmail.com>
2022-12-02 02:55:20 +0300

    Finished Lab3

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -1,11 +1,20 @@
-﻿internal class Program
+﻿using Lab3;
+
+internal class Program
 {
-    private static double 
+    private const double SiO2Volume = 1.0;
+
+    private static double _clConcentration;
+    private static double ClConcentration
+    { 
+        get => _clConcentration * Math.Pow(10, 18); 
+        set => _clConcentration = value; 
+    }
 
     private static void Main(string[] args)
     {
         Console.WriteLine("Для подсчёта требуется ввод данных");
-        Console.WriteLine("Нажмите [1], чтобы выбрать значения варианта 25. Или [AnyKey], чтобы ввести значения вручную");
+        Console.WriteLine("Нажмите [1], чтобы выбрать значения варианта 25. Или [OtherKey], чтобы ввести значения вручную");
 
         ConsoleKeyInfo readKey = Console.ReadKey(intercept: true);
         if (readKey.Key is ConsoleKey.D1 or ConsoleKey.NumPad1)
@@ -16,15 +25,23 @@
         {
             InputManually();
         }
+
+        double countOfAtomsSio2 = SiO2.CalculateCountOfAtoms(SiO2Volume);
+        Console.WriteLine($"Количество атомов SiO2 = {countOfAtomsSio2}");
+
+        double percentOfClInSiO2 = SiO2.CalculatePrecentOfElementInTape(ClConcentration, countOfAtomsSio2);
+        Console.WriteLine($"Процентное содержание Cl(%) в плёнке SiO2 = {percentOfClInSiO2}%");
     }
 
     private static void InputManually()
     {
-        
+        Console.Write("Введите концентрацию хлора в начальных слоях осаждённой плёнки  SiO2 (C) [см^-3]: ");
+        ClConcentration = Convert.ToDouble(Console.ReadLine());
     }
 
     private static void SetVariantValues()
     {
-        
+        const double VariantConcentration = 4.0;
+        ClConcentration = VariantConcentration;
     }
 }
---
+++ Lab3/SiO2.cs
@@ -0,0 +1,13 @@
+﻿namespace Lab3;
+
+public class SiO2
+{
+    private const double MolarVolume = 23.1;
+    private const double AvogadroNumber = 6.022E23;
+
+    public static double CalculateCountOfAtoms(double volume) 
+        => (volume / MolarVolume) * AvogadroNumber * 3.0;
+
+    public static double CalculatePrecentOfElementInTape(double ElementConcentration, double SiO2countOfAtoms)
+        => ElementConcentration / SiO2countOfAtoms * 100;
+}
