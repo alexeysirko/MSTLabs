990f575684d9ed82e51d4502cff671a425ec1e9a
Aleksey Sirko <leshasirko@gmail.com>
2022-12-09 06:30:42 +0300

    Finished Lab5

---
+++ Lab5/Program.cs
@@ -0,0 +1,35 @@
+﻿using Lab5;
+
+internal class Program
+{
+    private static double _volumeFlow;
+    private static Reactor _reactor = new();
+
+    private static void Main(string[] args)
+    {
+        SetVariantValues();
+
+        double linearFlow = _volumeFlow / _reactor.AreaBetweenSubstratesAndWalls;
+        Console.WriteLine($"Линейная скорость расхода (U) = {linearFlow} [см/мин]");
+
+        double reactorHeight = _reactor.CalculateLength();
+        Console.WriteLine($"Длина реактора (h) = {reactorHeight} [см]");
+
+        double timeInReactor = reactorHeight / linearFlow;
+        Console.WriteLine($"Длительность пребывания газа в реакторе (t): {timeInReactor} [мин]");
+    }
+
+    private static void SetVariantValues()
+    {
+        Console.WriteLine("Установлены значения варианта 25");
+        _volumeFlow = 602;
+        Console.WriteLine($"Объемный расход (V): {_volumeFlow} [см^3/мин]");
+
+        _reactor.Capacity = 68;
+        Console.WriteLine($"Объём реактора (L): {_reactor.Capacity} [л]");
+        _reactor.Diameter = 270;
+        Console.WriteLine($"Диаметр реактора (D): {_reactor.Diameter} [см]");
+        _reactor.AreaBetweenSubstratesAndWalls = 165;
+        Console.WriteLine($"Площадь между подложками и стенками реактора (S): {_reactor.AreaBetweenSubstratesAndWalls} [см^2]");
+    }
+}
---
+++ Lab5/Reactor.cs
@@ -0,0 +1,17 @@
+﻿namespace Lab5
+{
+    internal class Reactor
+    {
+        public double Capacity { get; set; }
+        public double Diameter { get; set; }
+        public double AreaBetweenSubstratesAndWalls { get; set; }
+
+
+        public double CalculateLength() => GetCapasityInCubeSm * GetSectionalArea;
+
+        private double GetCapasityInCubeSm => Capacity * 1000; 
+        private double GetSectionalArea => 4 / (Math.PI * Math.Pow(Diameter, 2));
+
+
+    }
+}
